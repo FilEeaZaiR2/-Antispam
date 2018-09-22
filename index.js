04bc69de97ee6d5e1eea0e3f687e515dc75da8ee
FilEeaZaiR2 <42357570+FilEeaZaiR2@users.noreply.github.com>
2018-09-22 14:13:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 
 var prefix = "!as";
 
-client.login("NDkzMDA5MDE5ODA4Nzc2MjA0.DoevXg.jvyp4WOyZ0elE0MMX_0BGkkF98o");
+client.login(process.env.TOKEN);
 
 client.on("ready", () => {
     console.log("Connexion en cours ...");
